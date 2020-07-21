108a1e3389b83660b41b1eb3091d1056c6ec00a4
Jovan95 <jovan95vujicic@gmail.com>
2020-07-21 16:56:21 +0200

    Add homepage Header.

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 // import PropTypes from 'prop-types';
-
+import logo from '../../images/logo.png';
+import homepage from '../../images/homepage.png';
 import './HomePage.scss';
 
 const HomePage = () => (
-  <div>HomePage</div>
+  <div className="homepage">
+    <div className="header">
+      <div className="header-left">
+        <div className="logo">
+          <img src={logo} alt="logo" className="logo" />
+        </div>
+        <div className="grid-item">
+          <Link to="/">O nama</Link>
+        </div>
+        <div className="grid-item">
+          <Link to="/">Advokati</Link>
+        </div>
+        <div className="grid-item">
+          <Link to="/">Oblasti</Link>
+        </div>
+        <div className="grid-item">
+          <Link to="/">Kontakt</Link>
+        </div>
+        <div className="grid-item" />
+        <div className="grid-item">
+          eng|srb
+        </div>
+        <div className="grid-item">
+          <div className="title">MILOS LEKIC</div>
+          <p className="title-about">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+      <div className="header-right">
+        <div className="image">
+          <img src={homepage} className="header-img" alt="homepage" />
+        </div>
+        <div className="box" />
+      </div>
+    </div>
+  </div>
 );
 
 HomePage.propTypes = {};
