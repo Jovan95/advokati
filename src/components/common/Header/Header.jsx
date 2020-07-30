911294ff22723dd80071321896fe2729f2218885
Jovan95 <jovan95vujicic@gmail.com>
2020-07-30 12:40:24 +0200

    Add header active link styling.

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -14,12 +14,12 @@ const Header = ({ location: { pathname } }) => {
       <div className="content-wrapper">
         <div className={pathname === '/' || pathname === '/about-us' ? 'nav-wrapper' : 'nav-wrapper-gray'}>
           <Link to="/" className="nav-item"><img className="headerLogo" src={headerLogo} alt="headerLogo" /></Link>
-          <Link to="/about-us" className="nav-item">o nama</Link>
-          <Link to="/lawyers" className="nav-item">advokati</Link>
-          <Link to="/areas" className="nav-item">oblasti</Link>
-          <Link to="/contact" className="nav-item">kontakt</Link>
+          <Link to="/about-us" className={pathname === '/about-us' ? 'nav-item active' : 'nav-item'}>o nama</Link>
+          <Link to="/lawyers" className={pathname === '/lawyers' ? 'nav-item active' : 'nav-item'}>advokati</Link>
+          <Link to="/areas" className={pathname === '/areas' ? 'nav-item active' : 'nav-item'}>oblasti</Link>
+          <Link to="/contact" className={pathname === '/contact' ? 'nav-item active' : 'nav-item'}>kontakt</Link>
           {
-            pathname !== '/' && pathname === '/about-us' && (
+            pathname !== '/' && '/about-us' && (
             <div className="line-wrapper">
               <div className="small-line" />
               <div className="small-line" />
---
+++ src/constants/headerLink.js
@@ -0,0 +1,22 @@
+export const HEADER_LINKS = [
+  {
+    id: 1,
+    path: 'about-us',
+    name: 'o nama',
+  },
+  {
+    id: 2,
+    path: 'lawyers',
+    name: 'advokati',
+  },
+  {
+    id: 3,
+    path: 'areas',
+    name: 'oblasti',
+  },
+  {
+    id: 4,
+    path: 'contact',
+    name: 'kontakt',
+  },
+];
