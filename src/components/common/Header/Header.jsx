1c82e4051655621815c7fb3a8c425f0457e530fa
Jovan Vujicic <jovan95vujicic@gmail.com>
2020-08-18 13:21:35 +0200

    Add responsive styling for Footer.

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
@@ -7,16 +7,27 @@ import headerLogo from '../../../images/headerLogo.png';
 import './Header.scss';
 
 const Header = ({ location: { pathname } }) => {
-  console.log(pathname);
+  const [checked, setChecked] = useState(false);
+  const closeMenu = () => { if (checked) setChecked(false); };
+
+  const changeChecked = () => {
+    setChecked(!checked);
+  };
   return (
-    <div className="header">
+    <div className={`header ${checked ? 'open' : ''}`}>
+
+
       <div className="content-wrapper">
+
+        <input className="menu-btn" onChange={() => {}} type="checkbox" id="menu-btn" checked={checked} />
+        <label className="menu-icon" htmlFor="menu-btn" onClick={changeChecked}><span className="navicon" /></label>
+
         <div className={pathname === '/' || pathname === '/about-us' ? 'nav-wrapper' : 'nav-wrapper-gray'}>
-          <Link to="/" className="nav-item"><img className="headerLogo" src={headerLogo} alt="headerLogo" /></Link>
-          <Link to="/about-us" className={pathname === '/about-us' ? 'nav-item active' : 'nav-item'}>o nama</Link>
-          <Link to="/lawyers" className={pathname === '/lawyers' ? 'nav-item active' : 'nav-item'}>advokati</Link>
-          <Link to="/areas" className={pathname === '/areas' ? 'nav-item active' : 'nav-item'}>oblasti</Link>
-          <Link to="/contact" className={pathname === '/contact' ? 'nav-item active' : 'nav-item'}>kontakt</Link>
+          <Link onClick={closeMenu} to="/" className="nav-item"><img className="headerLogo" src={headerLogo} alt="headerLogo" /></Link>
+          <Link onClick={closeMenu} to="/about-us" className={pathname === '/about-us' ? 'nav-item active' : 'nav-item'}>o nama</Link>
+          <Link onClick={closeMenu} to="/lawyers" className={pathname === '/lawyers' ? 'nav-item active' : 'nav-item'}>advokati</Link>
+          <Link onClick={closeMenu} to="/areas" className={pathname === '/areas' ? 'nav-item active' : 'nav-item'}>oblasti</Link>
+          <Link onClick={closeMenu} to="/contact" className={pathname === '/contact' ? 'nav-item active' : 'nav-item'}>kontakt</Link>
           {
             pathname !== '/' && '/about-us' && (
             <div className="line-wrapper">
