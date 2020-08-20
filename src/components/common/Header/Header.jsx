af88de4f413cd22f930ee5a9a25a3dbd91b26b3e
Jovan Vujicic <jovan95vujicic@gmail.com>
2020-08-20 13:39:16 +0200

    Add responsive styling for Header.

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
@@ -7,23 +7,47 @@ import headerLogo from '../../../images/headerLogo.png';
 import './Header.scss';
 
 const Header = ({ location: { pathname } }) => {
+  const scrollPos = useRef(0);
+  const visible = useRef(true);
+  const [visibleState, setVisibleState] = useState(true);
+
   const [checked, setChecked] = useState(false);
+
+  const changeChecked = () => { setChecked(!checked); };
   const closeMenu = () => { if (checked) setChecked(false); };
 
-  const changeChecked = () => {
-    setChecked(!checked);
-  };
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.innerWidth < 650) {
+        let currentScrollPos = window.pageYOffset;
+        console.log('scroll-cur:', currentScrollPos, 'scroll:', scrollPos);
+        if (currentScrollPos < 0) currentScrollPos = 0;
+
+        const newVisible = scrollPos.current >= currentScrollPos;
+
+        visible.current = newVisible;
+        scrollPos.current = currentScrollPos;
+
+        if (newVisible !== visible) setVisibleState(newVisible);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  });
+
   return (
-    <div className={`header ${checked ? 'open' : ''}`}>
+    <div className="header">
 
 
-      <div className="content-wrapper">
+      <div className={`content-wrapper ${!visibleState ? 'hidden' : ''} ${checked ? 'open' : ''}`}>
+        <Link onClick={closeMenu} to="/" className="nav-item"><img className="headerLogo" src={headerLogo} alt="headerLogo" /></Link>
 
         <input className="menu-btn" onChange={() => {}} type="checkbox" id="menu-btn" checked={checked} />
         <label className="menu-icon" htmlFor="menu-btn" onClick={changeChecked}><span className="navicon" /></label>
 
         <div className={pathname === '/' || pathname === '/about-us' ? 'nav-wrapper' : 'nav-wrapper-gray'}>
-          <Link onClick={closeMenu} to="/" className="nav-item"><img className="headerLogo" src={headerLogo} alt="headerLogo" /></Link>
           <Link onClick={closeMenu} to="/about-us" className={pathname === '/about-us' ? 'nav-item active' : 'nav-item'}>o nama</Link>
           <Link onClick={closeMenu} to="/lawyers" className={pathname === '/lawyers' ? 'nav-item active' : 'nav-item'}>advokati</Link>
           <Link onClick={closeMenu} to="/areas" className={pathname === '/areas' ? 'nav-item active' : 'nav-item'}>oblasti</Link>
