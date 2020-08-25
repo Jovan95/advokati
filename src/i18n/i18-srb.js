8036fe8a3839fa5d14bf37ca986c5e83fe0ee54c
Jovan Vujicic <jovan95vujicic@gmail.com>
2020-08-25 10:49:02 +0200

    Add i18-srb(serbian translation object)

---
+++ src/i18n/i18-srb.js
@@ -0,0 +1,99 @@
+/* eslint-disable */
+export default {
+  common: {
+      name: 'IME',
+      learn: 'SAZNAJ VISE',
+      area: 'ime oblasti',
+      number: 'BROJ TELEFONA',
+      message: 'VASA PORUKA',
+      send: 'POSALJI',
+      sending: 'SALJE SE',
+      title: 'o nama',
+  },
+  constants: {
+    areas: {
+      1: {
+        title: 'Krivicno Pravo',
+        text1: `Za odbranu je svakako od značaja i novina koja se ogleda u prikupljanju dokaza i predlaganju istih. Prema aktuelnom zakonodavstvu, odbrana kao i okrivljeni kao stranke u postupku imaju prava da predlažu dokaze koje idu u korist okrivljenom, ali sve dokaze na kojima se zasniva odbrana okrivljenog su dužni da predlože najkasnije na pripremnom ročištu ili na prvom glavnom pretresu ukoliko pripremno ročište nije održano, uz upozorenje suda da se neće izvoditi oni dokazi koji su bili poznati, a nisu predloženi u ovom periodu. U ovakvoj situaciji je od velikog značaja da efikasnost branioca bude na visini zadatka kako se ne bi ugrozila odbrana okrivljenog koja bi imala za posledicu nepovoljniji položaj samog okrivljenog u toku trajanja krivičnog postupka.`,
+        text2: 'Aktuelni Zakonik o krivičnom postupku predviđa još brojne novine kojima se daje značajna uloga braniocima u toku trajanja istrage kao i u toku trajanja krivičnih postupaka. Ono što je sigurno je to da najvažniji interes svakog branioca treba da bude pružanje najdelotvornije odbrane okrivljenom pred organima postupka uz potpuno raspolaganje stručnog znanja i veština.',
+      },
+      2: {
+        title: 'Korporativno Pravo',
+        text1: ` Privredno pravo čini zapravo skup pravnih normi o pravnom položaju privrednih subjekata ali i njihovom pravnom poslovanju na tržištu. Konkretno se privredno pravo odnosi na pitanja regulisanja pravnog položaja preduzeća i drugih subjekata koji se pojavljuju u privredi, kao i njihovim odnosima u pravnom prometu. Usluge advokatske kancelarije obuhvataju ne samo generalno usklađivanje poslovanja privrednih subjekata sa važećim propisima, već i zastupanje u konkretnim oblastima.`,
+        text2: ``,
+      },
+      3: {
+        title: 'Naknada Stete',
+        text1: `Pod pojmom štete podrazumeva se umanjenje nečije imovine (obična šteta) i sprečavanje njenog povećanja (izmakla korist), kao i nanošenje fizičkog ili psihičkog bola ili straha drugome (nematerijalna šteta). Angažovanjem advokata koji će zastupati vaše interese postižete da se bez direktnog angažovanja obeštetite odnosno dobijete adekvatnu novčanu nadoknadu u slučajevima kada je jedno lice (štetnik) izvršilo neki vid povrede prava drugog lica (oštećenog) u vidu prouzrokovanja bilo materijalne ili ne materijalne štete.`,
+        text2: `Advokatska kancelarija se bavi i ima iskustva sa naplatom komunalnih usluga (naplata dugova od potrošača kako komercijalnih tako i fizičkih lica). Takođe advokatska kancelarija je vršila i aktivno učestvovala u eksproprijaciji zemljišta (javni interes- gasifikacija) na teritoriji Bosne i Hercegovine, Republika Srpska i učestvovala u postupcima pred “Komisijom za koncesije Republike Srpske”. `,
+      },
+      4: {
+        title: 'Opsta Parnica',
+        text1: `Danas građansko pravo predstavlja najpre porodicu odnosno familiju više grana prava a koje čine: opšti deo građanskog prava, stvarno pravo, porodično pravo i obligaciono pravo pa je samim tim građansko pravo zasnovano na različitim načelima. Kao specijalizovana advokatska kancelarija za građansko pravo naš zadatak je najpre da upoznamo svoje klijente sa njihovim zakonskim pravima i obavezama u okviru parničnog postupka koji je pokrenut protiv njih ili postupka koje su pokrenuli protiv drugih lica.`,
+        text2: ` Nakon što se u potpunosti upoznamo sa predmetom, uz primenu zakonskih regulativa koji važe za konkretan predmet odnosno slučaj, klijentu predočavamo i predlažemo moguće načine za postupanje te u skladu sa tim preduzimamo sve neophodne pravne mere.`,
+      },
+      5: {
+        title: 'Porodicno Pravo',
+        text1: `Porodičnim pravom se uređuju najpre brak i bračni odnosi, zatim odnosi u vanbračnoj zajednici, odnosi deteta i roditelja, usvajanje, hraniteljstvo i starateljstvo, izdržavanje, imovinski odnosi u porodici, zaštita od nasilja u porodici, postupci u vezi sa porodičnim odnosima i lično ime. Ukoliko niste u mogućnosti da se samostalno dogovorite sa svojim supružnikom ili vanbračnim partnerom oko svih aspekata vezanih za razvod braka ili pak oko podele starateljstva nad detetom, tu smo da vam pomognemo u cilju postizanja mirnog rešenja.`,
+        text2: ``,
+      },
+      6: {
+        title: 'Ljudska prava',
+        text1: `Pored navedenog, naša advokatska kancelarija pruža i usluge sačinjavanja kupoprodajnih ugovora u prometu nekretnina i detaljne provere prodavca, njihovog poslovanja i postojanja eventualnih rizika i ugroženosti naših klijenata u svojstvu kupaca. Provere koje vršimo daju izuzetno visok nivo pravne sigurnosti prilikom kupovine nepokretnosti, a tiču se provere kako prodavca, tako i nepokretnosti u svim nadležnim sudovima i državnim organima.`,
+        text2: `Ta provera se sastoji od provere postojanja predstojećih ili postojećih izvršnih postupaka koji se vode protiv prodavca, te i provera kod Agencije za privredne registre i Narodne banke Srbije, a potom provere postojanja tereta i drugih pravni smetnji za prometovanje ili kasnije knjiženje prometovane nepokretnosti.`,
+      },
+    },
+  },
+  header: {
+    lawyers: 'advokati',
+    areas: 'oblasti',
+    contact: 'kontakt',
+  },
+  homepage: {
+    motto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    about: {
+      text1: `Advokat Milos Lekić rođen je 10.12.1981. godine u Beogradu. Završio je osnovnu i srednju školu u Beogradu. 2000 godine upisuje Pravni fakultet u Beogradu a koji završava 2005 godine. i nakon završetka osnovnih studija edukaciju i usavršavanje nastavlja na Univerzitetu Berkley USA i u drugim obrazovnim institucijama u Sjedinjenim američkim državama gde je i magistrirao na trgovinskom pravu u međunarodnom poslovanju 2008 godine. Bio je najmladji advokat koji je ikada postupao-branio pred Višim sudom u Beogradu-posebno odeljenje Višeg suda okrivljene od osnivanja specijalnog suda u Beogradu 2003 godine za krivična dela iz oblasti privrednog kriminala. Advokaturom se bavi od 2009 godine te je član advokatske komore Beograda i advokatske komore Srbije.`,
+      text2: `Advokat Miloš Lekić poseduje potrebne Sertifikate i Licence za odbranu maloletnih lica u postupcima u Republici Srbiji, za zastupanje oštećenih maloletnih lica a može postupati u svim krivičnim, parničnim, vanparničnim, upravnim i drugim predmetima i u svim drugim oblastima advokatskog rada u Republici Srbiji. Advokatska kancelarija Miloša Lekića danas ima još tri stalno zaposlena advokata te četiri advokatska pripravnika. Advokatska kancelarija je danas specijalizovana za pružanje usluga iz oblasti privrednog, korporativnog prava kao i prava iz oblasti ustavnih garancija fizičkih lica kao pojedinaca te njihove zaštite u ostvarivanju navedenih prava pred domaćim I međunarodnim sudovima.`,
+      text3: `Takođe zastupnici smo, i zastupali pravna lica (Zastava automobili ad u stečaju, Magnohrom doo u stečaju, Jugoeksport korporacija u stečaju deoničarsko društvo, Prvi maj Pirot ad u stečaju, Elektroporcelan ad u stečaju, Pobeda Promet ad u stečaju, BD agro ad u stečaju PKB voćarske plantaže u stečaju) i drugih posebnih pravnih subjekata ...`,
+    },
+    team: {
+      area: 'Oblasti/Ekspertize:',
+      title: 'nas tim',
+    },
+    areas: {
+      title: 'Oblasti/Ekspertize',
+
+    },
+    contact: {
+      title: 'kontakt',
+      comp: 'ime firme',
+      address: 'adresa',
+      phone: 'broj telefona',
+      write: 'pisite nam',
+      write_about: 'Stojimo vam na raspolaganju za sva dodatna pitanja',
+    }
+  },
+  about_us: {
+    content:{
+      page: 'kratak istorijat i opis poslova',
+      office: 'osnivac i vlasnik advokatske kancelarije lekic',
+      owner: 'advokat milos lekic',
+      text: `Rođen je 10.12.1981. godine u Beogradu. Završio je osnovnu i srednju školu u Beogradu. 2000 godine upisuje Pravni fakultet u Beogradu a koji završava 2005 godine. i nakon završetka osnovnih studija edukaciju i usavršavanje nastavlja na Univerzitetu Berkley USA i u drugim obrazovnim institucijama u Sjedinjenim američkim državama gde je i magistrirao na trgovinskom pravu u međunarodnom poslovanju 2008 godine. Bio je najmladji advokat koji je ikada postupao-branio pred Višim sudom u Beogradu-posebno odeljenje Višeg suda okrivljene od osnivanja specijalnog suda u Beogradu 2003 godine za krivična dela iz oblasti privrednog kriminala. Advokaturom se bavi od 2009 godine te je član advokatske komore Beograda i advokatske komore Srbije.`,
+      text2: `Advokat Miloš Lekić poseduje potrebne Sertifikate i Licence za odbranu maloletnih lica u postupcima u Republici Srbiji, za zastupanje oštećenih maloletnih lica a može postupati u svim krivičnim, parničnim, vanparničnim, upravnim i drugim predmetima i u svim drugim oblastima advokatskog rada u Republici Srbiji. Advokatska kancelarija Miloša Lekića danas ima još tri stalno zaposlena advokata te četiri advokatska pripravnika. Advokatska kancelarija je danas specijalizovana za pružanje usluga iz oblasti privrednog, korporativnog prava kao i prava iz oblasti ustavnih garancija fizičkih lica kao pojedinaca te njihove zaštite u ostvarivanju navedenih prava pred domaćim I međunarodnim sudovima.`,
+      text3: `Takođe zastupnici smo, i zastupali pravna lica (Zastava automobili ad u stečaju, Magnohrom doo u stečaju, Jugoeksport korporacija u stečaju deoničarsko društvo, Prvi maj Pirot ad u stečaju, Elektroporcelan ad u stečaju, Pobeda Promet ad u stečaju, BD agro ad u stečaju PKB voćarske plantaže u stečaju) i drugih posebnih pravnih subjekata na teritoriji Republike Srbije (pravna lica u restruktuiranju i stečaju). Kancelarija je bila 2015-2017 godine stručni savetnik RS-Agencije za licenciranje stečajni upravnika (ALSU), RS- Agencije za bezbednost saobraćaja (ABS) 2014-2015 godine, lokalne samouprave (grad Zaječar 2013-2016 godine) gde je aktivno učestvovala na izradi i primeni podzakonskih akata kao i izrade projekata javno privatnog partnerstva (PPI) kako sa domaćim tako i međunarodnim investitorima. Kancelarija posebno ima veliko iskustvo u radu državne i lokalne samouprave na teritoriji Republike Srbije gde je aktivno učestvovala u povećanju efikasnosti i konsolidacije kako državnih tako i organa lokalne samouprave na teritoriji RS.`,
+      text4: `Advokatska kancelarija Lekić danas zastupa velikih broj stranih (VICTOR GROUP GMBH; FUJITSU-SIEMENS; PACOM INTERNACIONAL; PLASTIC AD; SHANGHAI ELECTRIC; BJELJINA GAS DOO) i domaćih pravnih lica (ADP DOO; GRAFO MARKET DOO; DP NOVI SAD GAS) takođe advokatska kancelarija poseduje veliko iskustvo u oblasti bankarstva i naplate potraživanja u korist banaka i drugih pravnih lica (kancelarija zastupa/la AGROBANKU AD kao i NOVU AGROBANKU AD u postupku stečaja kao i naplate dospelih i neizmirenih obaveza kako od strane pravnih tako i od strane fizičkih lica). Advokat Miloš Lekić je bio predsednik i član Upravnog odbora privrednog društva “Društveno preduzeće Novi Sad GAS” 2018-2019 godine (najveći distributer prirodnog gasa krajnjim korisnicima na teritoriji RS). Advokatska kancelarija se bavi i ima iskustva sa naplatom komunalnih usluga (naplata dugova od potrošača kako komercijalnih tako i fizičkih lica). Takođe advokatska kancelarija je vršila i aktivno učestvovala u eksproprijaciji zemljišta (javni interes-gasifikacija) na teritoriji Bosne i Hercegovine, Republika Srpska i učestvovala u postupcima pred “Komisijom za koncesije Republike Srpske”.`,
+      text5: `Posebno napominjemo veliko iskustvo u postupcima pred “Konkurencijskim savjetom” Bosne I Hercegovine i to u postupcima koje je to telo kao nezavisni organ pokretalo protiv pravnih subjekata zbog sprečavanja, ograničavanja ili narušavanja tržišne konkurencije na teritoriji Bosne i Hercegovine kao i izvan teritorije BIH a koje su imale uticaja na teritoriju iste. Kancelarija je učestvovala i u zaključivanju sporazuma (pravna regulativa) između pravnih lica-privrednih subjekata a koji kao takvi mogli biti ili su bili predmet ispitivanja od strane “Konkurencijskog savjeta” Bosne i Hercegovine. Posebno posedujemo dugogodišnje iskustvo u zastupanju i zaštiti ljudskih i manjinskih prava pred Ustavnim sudom RS i međunarodnim sudom pravde u Strazburu. Pružamo pravnu zaštitu i saradnju licima sa posebnim potrebama (pro bono) kao i zaštitu prava maloletnika i žena u sudskim i upravnim postupcima te krivičnim postupcima kao punomoćnici oštećenih za krivična dela iz oblasti nasilja u porodici. Do sada smo učestvovali i pravno pomagali u osnivanju i praćenju rada organizacija iz nevladinog sektora i udruženjima a koja se bave zaštitom od nasilja u porodici kao i organizacijama koja se bave izjednačavanjem položaja oba pola a u skladu sa Ustavom RS i drugim pozitivno-pravnim propisima koji prate navedenu materiju. `,
+    },
+  },
+  lawyers: {
+    title: 'advokati',
+    options: {
+      label1: 'Krivicno Pravo',
+      label2: 'Korporativno Pravo',
+      label3: 'Naknada Stete',
+      label4: 'Opsta Parnica',
+      label5: 'Porodicno Pravo',
+      label6: 'Ljudska prava',
+    }
+  }
+};
